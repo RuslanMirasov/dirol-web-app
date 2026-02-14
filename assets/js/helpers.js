db03becfc95afeeea96577de443058de213fa9a2
Ruslan <info@mirasov.dev>
2026-02-14 15:46:20 +0100

    add main page

---
+++ assets/js/helpers.js
@@ -0,0 +1,36 @@
+export const throttle = (func, limit) => {
+  let lastCall = 0;
+  return function (...args) {
+    const now = Date.now();
+    if (now - lastCall >= limit) {
+      lastCall = now;
+      func.apply(this, args);
+    }
+  };
+};
+
+export const debounce = (func, delay) => {
+  let timeoutId;
+  return function (...args) {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(this, args);
+    }, delay);
+  };
+};
+
+export const initNavigationMenu = () => {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.navigation ');
+  const menuLinks = document.querySelectorAll('.menu__link');
+
+  if (!burger) return;
+
+  const toggleMenu = () => {
+    burger.classList.toggle('open');
+    menu.classList.toggle('open');
+  };
+
+  if (burger) burger.addEventListener('click', toggleMenu);
+  menuLinks.forEach(link => link.addEventListener('click', toggleMenu));
+};
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,7 @@
 import { quizStore } from './quiz.js';
+import { initNavigationMenu } from './helpers.js';
 
 window.quiz = quizStore();
 window.quiz.init();
+
+initNavigationMenu();
