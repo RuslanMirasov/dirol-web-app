5745260cd700e6e0cf77f804ef4d8f2eaaf7afe0
Ruslan <info@mirasov.dev>
2026-02-15 22:16:19 +0100

    add winners and lk pages

---
+++ assets/js/forms.js
@@ -0,0 +1,157 @@
+const showErrors = true;
+
+export const initSelectFields = () => {
+  const allSelectEl = document.querySelectorAll('[data-select]');
+
+  if (allSelectEl.length === 0) return;
+
+  allSelectEl.forEach(select => {
+    new Choices(select, {
+      searchEnabled: false,
+      shouldSort: false,
+    });
+  });
+};
+
+const validationRegEx = [
+  {
+    type: 'email',
+    regex: /^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/,
+    error: 'Не верный формат E-mail!',
+  },
+  {
+    type: 'url',
+    regex: /^(https?:\/\/)?([\w.-]+\.)+[a-zA-Z]{2,}(\/\S*)?$/,
+    error: 'Не верный формат URL',
+  },
+];
+
+const validateInput = input => {
+  if (!input.required) return;
+
+  const validationError = error => {
+    addErrorHTML(error, input);
+    return false;
+  };
+
+  const { name, value, checked, type } = input;
+
+  if ((type === 'checkbox' || type === 'radio') && !checked) {
+    return validationError(validationRegEx.find(rule => rule.type === type).error);
+  }
+
+  if (!value || value === '') {
+    return validationError('Это обязательное поле!');
+  }
+
+  const typeValidation = validationRegEx.find(v => v.type === type);
+
+  if (typeValidation) {
+    const regex = new RegExp(typeValidation.regex);
+
+    if (!regex.test(value.trim())) {
+      return validationError(typeValidation.error);
+    }
+  }
+
+  const nameValidation = validationRegEx.find(v => v.name === name);
+
+  if (nameValidation) {
+    const regex = new RegExp(nameValidation.regex);
+
+    if (!regex.test(value.trim())) {
+      return validationError(nameValidation.error);
+    }
+  }
+
+  removeErrorHTML(input);
+  return true;
+};
+
+const validateForm = form => {
+  if (!form) return;
+  let errorsCount = 0;
+
+  const inputs = form.querySelectorAll('[required]');
+  if (inputs.length === 0) return;
+
+  inputs.forEach(input => {
+    const isInputValid = validateInput(input);
+    errorsCount = isInputValid ? errorsCount : errorsCount + 1;
+  });
+
+  return errorsCount <= 0;
+};
+
+const addErrorHTML = (error, input) => {
+  if (!input) return;
+
+  const label = input.closest('label');
+  const existingError = label?.querySelector('.inputError');
+
+  if (error) {
+    input.classList.add('invalid');
+
+    if (existingError) {
+      existingError.innerHtml = error;
+      return;
+    }
+
+    if (showErrors) {
+      label.insertAdjacentHTML('beforeend', `<span class="inputError"><span>${error}</span></span>`);
+      const newError = label.querySelector('.inputError');
+      newError.style.height = newError.scrollHeight + 'px';
+    }
+    return;
+  }
+
+  if (existingError) existingError.remove();
+  input.classList.remove('invalid');
+};
+
+const removeErrorHTML = input => {
+  if (!input) return;
+
+  const label = input.closest('label');
+  const error = label?.querySelector('.inputError');
+  input.classList.remove('invalid');
+  if (error) {
+    error.style.height = '0px';
+    setTimeout(() => {
+      error.remove();
+    }, 300);
+  }
+};
+
+const onRequiredInputFocus = e => {
+  const input = e.target;
+  removeErrorHTML(input);
+};
+
+document.addEventListener('focusin', e => {
+  if (e.target.matches('[required]')) {
+    onRequiredInputFocus(e);
+  }
+});
+
+document.addEventListener('change', e => {
+  if (e.target.type === 'checkbox') {
+    validateInput(e.target);
+  }
+});
+
+// SUBMIT MIDDLEWARE
+document.addEventListener(
+  'submit',
+  function (event) {
+    const form = event.target;
+    if (form.tagName.toLowerCase() !== 'form') return;
+
+    const isValid = validateForm(form);
+    if (!isValid) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+    }
+  },
+  true
+);
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 import { quizStore } from './quiz.js';
 import { initNavigationMenu, hidePreloader } from './helpers.js';
+import { initSelectFields } from './forms.js';
 import { initSliders } from './sliders.js';
 
 window.quiz = quizStore();
@@ -7,6 +8,7 @@ window.quiz.init();
 
 initNavigationMenu();
 initSliders();
+initSelectFields();
 
 window.addEventListener('load', () => {
   setTimeout(() => {
